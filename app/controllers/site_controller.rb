3911932752dca464657b7c782adc1e3a77fcb6f5
gthetwong <gthetwong@gmail.com>
2014-04-01 12:36:47 -0700

    Adding in comments for each function. Adding function to give markers more detail.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,16 +2,17 @@ class SiteController < ApplicationController
 include SessionsHelper
 
 	def index
-		if Event.all.empty? == false
+		if Event.all.empty? 
 			Event.load_info
 		else
 			Event.cleanse
 		  Event.load_info
 		end
 
-		@events = Event.get_lat
-		gon.locations = @events
-		
+		@events_locations = Event.get_lat
+		gon.locations = @events_locations
+		@events_marker_info = Event.marker_data
+		binding.pry
 	end
 
 	
